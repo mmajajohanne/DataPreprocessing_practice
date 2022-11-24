c0a7a3d9153e411dc47520962e13133f1c697e94
Maja Marjamaa <82090456+mmajajohanne@users.noreply.github.com>
2022-11-24 11:39:31 +0100

    Update TrianglePlot.cpp

--- TrianglePlot.cpp
+++ TrianglePlot.cpp
@@ -30,7 +30,8 @@ int main( void )
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); //We don't want the old OpenGL 
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); //We don't want the old OpenGL
+	
 
 	// Open a window and create its OpenGL context
 	window = glfwCreateWindow( 1024, 768, "Tutorial 03 - Matrices", NULL, NULL);
